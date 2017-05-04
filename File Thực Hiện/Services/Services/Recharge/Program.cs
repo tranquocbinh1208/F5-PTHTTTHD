45bfbce9024b05912dacea0e7d6e7ae3ca1d0169
Truong Mai <truongmv@outlook.com>
2017-05-04 13:56:26 +0700

    create project Services.sln \ Recharge service

---
+++ File Thực Hiện/Services/Services/Recharge/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Owin.Hosting;
+using System;
+
+namespace Recharge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string baseAddress = "http://localhost:9000/";
+
+            using (WebApp.Start<Startup>(url: baseAddress))
+            {
+                Console.WriteLine($"Recharge service was started at {baseAddress} \nPress any key to stop");
+                Console.ReadLine();
+            }
+        }
+    }
+}
