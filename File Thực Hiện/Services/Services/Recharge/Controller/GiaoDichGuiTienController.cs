3fc27ab40106c98fb0992e1dff35b2bef556862c
Truong Mai <truongmv@outlook.com>
2017-05-04 15:11:31 +0700

    implement Recharge feature with static data

---
+++ File Thực Hiện/Services/Services/Recharge/Controller/GiaoDichGuiTienController.cs
@@ -0,0 +1,70 @@
+﻿using Recharge.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace Recharge.Controller
+{
+    public class GiaoDichGuiTienController : ApiController
+    {
+        public static List<GiaoDichGuiTien> GiaoDichs = new List<GiaoDichGuiTien>();
+
+        [HttpGet]
+        public IEnumerable<GiaoDichGuiTien> LayTatCaGiaoDich()
+        {
+            return GiaoDichs;
+        }
+        [HttpGet]
+        public GiaoDichGuiTien LayGiaoDichTheoMaGD(string maGD)
+        {
+            if (!string.IsNullOrEmpty(maGD))
+                return GiaoDichs.Find(a => a.MaGD == maGD);
+
+            return null;
+        }
+        [HttpGet]
+        public IEnumerable<GiaoDichGuiTien> LayGiaoDichTheoTuKhoa(string tuKhoa)
+        {
+            return GiaoDichs.Where(a =>
+                (a.HoTenNguoiGoi + a.CMND + a.SDT + a.Diachi + a.SoTien + a.NoiDung)
+                .ToLower()
+                .Contains((tuKhoa ?? string.Empty).ToLower())
+            );
+        }
+        [HttpGet]
+        public IEnumerable<GiaoDichGuiTien> LayGiaoDichTheoNgay(DateTime tuNgay, DateTime denNgay)
+        {
+            if (tuNgay != null && denNgay != null)
+                return GiaoDichs.Where(a => a.NgayTao >= tuNgay && a.NgayTao <= denNgay);
+
+            return null;
+        }
+        [HttpPut]
+        public GiaoDichGuiTien ThemGiaoDich(GiaoDichGuiTien gd)
+        {
+            if (ModelState.IsValid && gd != null)
+            {
+                GiaoDichs.Add(gd);
+                return gd;
+            }
+
+            return null;
+        }
+        [HttpPost]
+        public GiaoDichGuiTien HuyGiaoDich(GiaoDichGuiTien gd)
+        {
+            if (ModelState.IsValid && gd != null)
+            {
+                var giaoDich = GiaoDichs.Where(a => a.MaGD == gd.MaGD).FirstOrDefault();
+                if (giaoDich != null)
+                {
+                    giaoDich.TrangThai = TrangThaiGiaoDich.Huy;
+                    return gd;
+                }
+            }
+
+            return null;
+        }
+    }
+}
---
+++ File Thực Hiện/Services/Services/Recharge/Model/GiaoDichGuiTien.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recharge.Model
+{
+    public enum TrangThaiGiaoDich
+    {
+        DangXuLy = 0,
+        ThanhCong = 1,
+        ThatBai = 2,
+        Huy = 3
+    }
+
+    public class GiaoDichGuiTien
+    {
+        [Key]
+        [Required]
+        public String MaGD { get; set; }
+        [Required]
+        public string HoTenNguoiGoi { get; set; }
+        [Required]
+        public string CMND { get; set; }
+        [Required]
+        public DateTime NgayCapCMND { get; set; }
+        [Required]
+        public string NoiCapCMND { get; set; }
+        [Required]
+        public string Diachi { get; set; }
+        public string SDT { get; set; }
+        [Required]
+        public decimal SoTien { get; set; }
+        [Required]
+        public string NoiDung { get; set; }
+        [Required]
+        public string MaKHNhan { get; set; }
+        [Required]
+        public string MaNV { get; set; }
+        [Required]
+        public DateTime NgayTao { get; set; }
+        [Required]
+        public TrangThaiGiaoDich TrangThai { get; set; }
+    }
+}
